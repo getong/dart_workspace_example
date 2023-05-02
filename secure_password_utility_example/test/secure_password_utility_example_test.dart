163b4b632b86e0dc85871ce20aed936b3b8fa32b
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-02 21:19:12 +0800

    add secure_password_utility example

---
+++ secure_password_utility_example/test/secure_password_utility_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:secure_password_utility_example/secure_password_utility_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
