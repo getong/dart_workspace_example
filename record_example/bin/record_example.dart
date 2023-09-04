c464cfe02114a3156807abfaf573253a919cc353
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-04 18:10:51 +0800

    add record example

---
+++ record_example/bin/record_example.dart
@@ -0,0 +1,8 @@
+import 'package:record_example/record_example.dart' as record_example;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${record_example.calculate()}!');
+
+  const record = (a: 1, b: true);
+  print('${record.a} ${record.b}');
+}
---
+++ record_example/test/record_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:record_example/record_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
