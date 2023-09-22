60c75017d5ea97c6604256c68b5034ec4a8c62ed
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-22 19:02:45 +0800

    add events emitter example

---
+++ events_emitter_example/bin/events_emitter_example.dart
@@ -0,0 +1,19 @@
+// import 'package:events_emitter_example/events_emitter_example.dart' as events_emitter_example;
+
+// ignore_for_file: avoid_print
+
+import 'package:events_emitter/events_emitter.dart';
+
+void main() {
+  final events = EventEmitter();
+
+  events.on('message', (String data) => print('String: $data'));
+  events.on('message', (int data) => print('Integer: $data'));
+
+  events.emit('message', 'Hello World');
+  events.emit('message', 42);
+
+  // [Output]
+  // String: Hello World
+  // Integer: 42
+}
---
+++ events_emitter_example/test/events_emitter_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:events_emitter_example/events_emitter_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
