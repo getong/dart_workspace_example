249bac1cb16c6d8d8c519b2fb531debcb15191d3
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-22 09:11:18 +0800

    add bloc transition example

---
+++ bloc_transition_example/bin/bloc_transition_example.dart
@@ -0,0 +1,54 @@
+// import 'package:bloc_transition_example/bloc_transition_example.dart' as bloc_transition_example;
+
+import 'package:bloc/bloc.dart';
+sealed class CounterEvent {}
+
+final class CounterIncrementPressed extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) => emit(state + 1));
+  }
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    super.onTransition(transition);
+    print(transition);
+  }
+}
+
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print('${bloc.runtimeType} $transition');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
+
+void main(List<String> arguments) async {
+  // print('Hello world: ${bloc_transition_example.calculate()}!');
+  Bloc.observer = SimpleBlocObserver();
+  CounterBloc()
+  ..add(CounterIncrementPressed())
+  ..close();
+}
---
+++ bloc_transition_example/test/bloc_transition_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_transition_example/bloc_transition_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
