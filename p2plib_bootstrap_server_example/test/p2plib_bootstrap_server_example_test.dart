2e23fb7b9c181d33a4ce11c768ebcce4934d6f92
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-28 17:10:29 +0800

    add p2plib bootstrap server example

---
+++ p2plib_bootstrap_server_example/test/p2plib_bootstrap_server_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:p2plib_bootstrap_server_example/p2plib_bootstrap_server_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
