7cdde68a0d9567964a8306d072b47abc57f7fd39
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-05-30 22:14:09 +0800

    add dart future example2

---
+++ dart_future_example2/bin/dart_future_example2.dart
@@ -0,0 +1,20 @@
+void main() async {
+  int myDelay = 5;
+
+  print ('Hello');
+
+  var value = await _customDelay(myDelay);
+  var customText = myDelay == 1 ? "second later": "seconds later";
+
+  print ('Its $value $customText');
+}
+
+Future <int> _customDelay(int delay) async {
+  try {
+    await Future.delayed(Duration(seconds: delay));
+    return delay;
+  } catch (e) {
+    print(e);
+    return delay;
+  }
+}
---
+++ dart_future_example2/test/dart_future_example2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_future_example2/dart_future_example2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
