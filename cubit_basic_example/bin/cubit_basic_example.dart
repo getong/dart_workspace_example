6ad114665dc7c348c1eba9b70597a5a3e2eced3a
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-06-02 02:27:27 +0800

    add async code

--- cubit_basic_example/bin/cubit_basic_example.dart
+++ cubit_basic_example/bin/cubit_basic_example.dart
@@ -1,18 +1,19 @@
 // import 'package:cubit_basic_example/cubit_basic_example.dart' as cubit_basic_example;
 import 'package:bloc/bloc.dart';
 
-class CounterCubit extends Cubit<int>{
+class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
-  void increment() => emit(state +1);
-  void decrement() => emit(state -1);
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
 }
 
-void main(List<String> arguments) {
+Future<void> main(List<String> arguments) async {
   // print('Hello world: ${cubit_basic_example.calculate()}!');
 
   final cubit = CounterCubit();
   print(cubit.state);
+  final subscription = cubit.stream.listen(print);
 
   cubit.increment();
   print(cubit.state);
@@ -20,5 +21,9 @@ void main(List<String> arguments) {
   print(cubit.state);
   cubit.increment();
   print(cubit.state);
+
+  await Future.delayed(Duration.zero);
+  await subscription.cancel();
+
   cubit.close();
 }
